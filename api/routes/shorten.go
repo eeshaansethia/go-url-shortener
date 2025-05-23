4e38faa24b6e34d2eaa5bf36a707bb5bf5bf97ca
Eeshaan Sethia <eeshaans05@gmail.com>
2025-05-23 14:55:44 +0530

    Impletementing Rate Limiting

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -1,10 +1,14 @@
 package routes
 
 import (
+	"go-url-shortener/database"
 	"go-url-shortener/helpers"
+	"os"
+	"strconv"
 	"time"
 
 	"github.com/asaskevich/govalidator"
+	"github.com/go-redis/redis/v8"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -28,6 +32,24 @@ func ShortenURL(c *fiber.Ctx) error{
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
 			"error": "Invalid request",
 		})
+	}
+
+	// Rate Limiting
+	r2 := database.CreateClient(1)
+	defer r2.Close()
+	val, err := r2.Get(database.Ctx, c.IP()).Result()
+	if err == redis.Nil {
+		err = r2.Set(database.Ctx, c.IP(), os.Getenv("API_QOUTA"), 30*60*time.Second).Err()
+	} else {
+		valInt, _ := strconv.Atoi(val)
+		if valInt <= 0 {
+			limit, _ := r2.TTL(database.Ctx, c.IP()).Result()
+			return c.Status(fiber.StatusTooManyRequests).JSON(fiber.Map{
+				"error": "Rate limit exceeded",
+				"rate_limit": limit,
+				"rate_limit_reset": limit/time.Nanosecond/time.Minute,
+			})
+		}
 	}
 
 	if !govalidator.IsURL(body.URL){
